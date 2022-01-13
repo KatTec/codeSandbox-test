f4f8f9362f74cbb9946f8fcae050fa5d1fac9410
KatTec <68884526+KatTec@users.noreply.github.com>
2022-01-14 08:45:29 +0900

    js動作検証

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-console.log("■■index.js start");
+import "./styles.css";
+
+const onClickAdd = () => {
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+  // alert(inputText);
+
+  // div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  // liタグ生成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+  // console.log(li);
+
+  // divタグの子要素に各要素を設定;
+  div.appendChild(li);
+  console.log(div);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
